6c953755b925c1047d0e173b80281811503322ae
enasmohmed <enasm2477@gmail.com>
2021-05-24 08:51:31 +0200

    add blog comment

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from django_summernote.admin import SummernoteModelAdmin
+
 from blog.models import Post, Category
 
-admin.site.register(Post)
+
+class SomeModelAdmin(SummernoteModelAdmin):
+    summernote_fields = '__all__'
+
+
+admin.site.register(Post, SomeModelAdmin)
 admin.site.register(Category)
