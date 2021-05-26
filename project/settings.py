b55370ada2957da3ab3e0f8330436cbb6a66c863
enasmohmed <enasm2477@gmail.com>
2021-05-26 10:34:56 +0200

    ready depoly heroku

--- project/settings.py
+++ project/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static", "staticroot")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     '/var/www/static/',
